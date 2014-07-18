9fa8f00286405ea5e994359b21b5bf15916b9405
Jeff Lambert <jefflikeschicken@gmail.com>
2014-07-17 20:34:52 -0700

    Adding readme, adding bower, documentation

---
+++ README.md
@@ -0,0 +1,70 @@
+# Retryable 
+
+Provides a mechanism to keep attempting something over and over until it is 
+successful. Provide it a function to keep calling until it returns true, and 
+it will keep attempting it until it does all in a non-blocking manner. 
+Optionally provide a maximum number attempts, a successful callback, or add 
+random variance to the sleep time between callbacks.
+
+A good use case for this would be real-time services that could, from time 
+time, require restart or maintenance while clients are connected. Upon restart 
+all clients would then become disconnected, and, by leveraging Retryable, 
+continuously attempt to reconnect until service is restored.
+
+The amount of time between attempts is between a user-supplied minimum and 
+maximum value. Should the callback function fail multiple times, the amount of 
+time between attempts will grow logarithmically until it reaches the maximum 
+value.  If random variance is specified, then the amount added between each 
+successive failed attempt will be between -sleepMin and +sleepMin.
+
+The random variance added, if used, is meant to prevent a large number of 
+clients from flooding the service with connection requests at the same time 
+when it becomes available.
+
+## Usage 
+
+```javascript
+   var tryThis = function() {
+        // 10 % success rate
+        return (Math.rand() < 0.1 ? true : false);
+    };
+
+    var success = function(wasSuccessful) {
+        if(wasSuccessful) {
+            alert('success!');
+        } else {
+            alert('Fail! Max Tries exceeded');
+        }
+    };
+
+    var sleepMin = 3,   // Sleep for a minimum of 3 and a maximum of 30 
+        sleepMax = 30;  // seconds between attempts.
+
+    var retry = new Retryable(sleepMin, sleepMax);
+
+    // Call tryThis forever until it is successful, no 
+    // notification of success.
+    retry.call(tryThis)
+        .begin()
+    ;
+
+    // Call until success, notify when successful.
+    retry.call(tryThis)
+        .then(notifyComplete)
+        .begin()
+    ;
+
+    // Make a maximum of 5 attempts
+    retry.call(tryThis)
+        .then(notifyComplete)
+        .max(5)
+        .begin()
+    ;
+
+    // Keep calling forever, but with a random variance
+    retry.call(tryThis)
+        .withVariance()
+        .then(notifyComplete)
+        .begin()
+    ;
+```
--- dist/retryable.js
+++ dist/retryable.js
@@ -1,3 +1,27 @@
+/**
+ * Retryable 
+ *
+ * Provides a mechanism to keep attempting something over and over until it is 
+ * successful. Provide it a function to keep calling until it returns true, and 
+ * it will keep attempting it until it does. Timeout between attempts is 
+ * calculated on a logarithmic scale, with an optional random variance applied.
+ *
+ * Copyright (C) 2014  Jeff Lambert
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ * 
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ */
 (function(w) {
     w.Retryable = Retryable;
 
@@ -42,7 +66,7 @@
          * Starts the callback loop
          */
         function begin() {
-            callbackLoop();
+            return callbackLoop();
         }
 
         //  }}}
@@ -64,23 +88,29 @@
         //  }}} 
         //  {{{ callbackLoop()
 
+        /**
+         * Continuously attempts to execute the callback, until the callback 
+         * returns a truthy value. Aborts if maxTries attempts fail.
+         *
+         * If a success callback is provided, it will be called once the 
+         * callback method is successful with the boolean value true as the 
+         * first parameter . Having maxTries attempts fail will also notify 
+         * the success callback, but with the boolean value false passed in as 
+         * the first parameter.
+         */
         function callbackLoop() {
             self.count++;
-            var nextSleepValue = getSleepValue(self.count);
+            var nextSleepValue = getSleepValue(self.count),
+                callbackResult = self.callback(self.count, nextSleepValue);
 
-            if(!self.callback(self.count, nextSleepValue)) {
-                if(self.maxTries && self.count >= self.maxTries) {
-                    self.success(false);
-                } else {
-                    setTimeout(
-                        callbackLoop, 
-                        nextSleepValue
-                    );
-                }
-            } else {
-                if(self.success) {
-                    self.success(true);
-                }
+            if(!callbackResult) {
+                if(!self.maxTries || (self.count < self.maxTries)) {
+                    setTimeout(callbackLoop, nextSleepValue);
+                } 
+            }
+
+            if(self.success) {
+                self.success(callbackResult);
             }
         }
 
@@ -116,6 +146,9 @@
          */
         function getVariance(scale) {
             return !self.variance ? 0 : 
+                // Generate random sign
+                (Math.random() < 0.5 ? -1 : 1) * 
+                // Random number between 0 and sleepMin
                 Math.ceil( sleepMin * Math.random() ) * 1000
             ;
         }
